51737ae765f39ed528409907d00349bb275cc4a0
Oleksandr Chernyshov <slvschool12@gmail.com>
2026-01-18 17:24:15 +0200

    Initial commit

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,42 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const { delay, state } = event.currentTarget.elements;
+  const delayValue = Number(delay.value);
+  const stateValue = state.value;
+
+  // Створюємо проміс
+  createPromise(delayValue, stateValue)
+    .then(delay => {
+      iziToast.success({
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    });
+    
+  // Очищення форми після створення промісу
+  form.reset();
+});
+
+function createPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
